d8fc4455226c466522c355fde1b0d93dac51aeb0
AmgadEmadAzmy <119006761+AmgadEmadAzmy@users.noreply.github.com>
2023-01-19 01:47:51 +0200

    Add files via upload

---
+++ loadbalncer_variables.tf
@@ -0,0 +1,36 @@
+variable "target_name" { 
+}
+variable "target_port" { 
+}
+variable "target_protocol" { 
+}
+variable "target_vpc_id" { 
+}
+
+
+variable "attach_target_id" { 
+}
+variable "attach_target_port" { 
+}
+
+
+
+variable "lb_name" {
+}
+variable "lb_internal" { 
+}
+variable "lb_type" {
+}
+variable "lb_security_group" { 
+}
+variable "lb_subnet" { 
+}
+
+
+
+variable "listener_port" { 
+}
+variable "listener_protocol" {
+}
+variable "listener_type" { 
+}
